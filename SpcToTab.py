cc9d20712f3cf40f851eb414f6154683359c51a2
ADVFRED <theadvfred@gmail.com>
2021-03-27 17:51:09 +0000

    get hash of previous commit

--- SpcToTab.py
+++ SpcToTab.py
@@ -3,7 +3,21 @@ import fileinput
 import subprocess
 
 def GetChangedFiles():
-	return subprocess.run(["git", "show"])
+	with open("out.txt","w") as out:
+		subprocess.run(["git", "show","-q"], stdout=out)
+	
+
+def CleanGitShow():
+	with open("out.txt", "r") as out:
+		lines=out.readlines()
+	with open("out.txt", "w") as out:
+		for line in lines:
+			if "commit" in line.strip("\n"):
+				out.write(line)
+				hash=line
+				hash=hash.replace("commit ","")
+	with open("out.txt","w") as out:
+		out.write(hash)
 
 def main(filename):
 	with open(filename, "r") as file:
@@ -12,5 +26,6 @@ def main(filename):
 	with open(filename, "w") as file:
 		file.write(filedata)
 if __name__ == "__main__":
-	print(GetChangedFiles())
+	GetChangedFiles()
+	CleanGitShow()
 	main("test.py")
